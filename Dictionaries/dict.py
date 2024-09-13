7605cb722f696fdb30cbc678ad84e8f33f3a3147
James Barn Mutwiri <mutwirijamesbarn@gmail.com>
2024-09-13 15:51:53 +0300

    Add file to new branch

---
+++ Dictionaries/dict.py
@@ -0,0 +1,14 @@
+#creating one
+animal={'mammal':'lion','Reptile':'Lizard','Amphibian':'Newt','Birds':'Eagle'}
+print(animal['mammal'])
+print(animal['Reptile'])
+print(animal['Amphibian'])
+
+newanimal=animal['Amphibian']
+print(f"We just discovered an {newanimal} on the farm")
+
+cars={'Toyota':'Auris','BMW':'X5','Mazda':'Demio','Isuzu':'Hilux'}
+print(cars['Mazda'])
+print(cars['Isuzu'])
+newcar=cars['Isuzu']
+print(f"Mem just bought a {newcar} truck"  )
