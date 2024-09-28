c9dea34f18726638a351abe4f0ab2031422cc5b1
Jamesbarn <108346960+Jamesbarn001@users.noreply.github.com>
2024-09-28 14:31:33 +0300

    Add files via upload
    Examples of working with lists on python

---
+++ Lists/sortlists.py
@@ -0,0 +1,40 @@
+cars=['Subaru','Range rover','Jaguar','toyota']
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+#sorted lists
+cars=['subaru','range rover','jaguar','toyota']
+print(sorted(cars))
+
+#Sorting
+places=['australia','kenya','rwanda','alaska','india']
+print(places[-3])
+print(len(places))
+print(places)
+print(sorted(places))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
+
+#exercise
+houses=['joshuas','kings','alphas','ravenhead','esau']
+print(houses)
+houses.sort()
+print(houses)
+houses.sort(reverse=True)
+print(houses)
+print(sorted(houses))
+houses.reverse()
+print(houses)
+
+
+places.sort()
+print(places)
---
+++ Lists/tupple.py
@@ -0,0 +1,14 @@
+dimensions = (200,49,80,90,85,56,76)
+
+for dimension in dimensions:
+    print(dimension)
+dimensions=(210,77,88,90,100,65,46,98)
+print(dimensions)
+
+cakes=('chocolate','red velvet','fruit','passion','vanilla')
+for cake in cakes:
+   
+    print(cake)
+cakes=('\nchocolate','red velvet','fruit','passion','vanilla','white velvet','black forest')
+for cake in cakes:
+    print(cake) 
